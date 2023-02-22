39b196943e2e2ffad3c476c913d04aad802e5389
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-02-22 19:51:31 +0300

    1-backend

--- constants/env.js
+++ constants/env.js
@@ -1,5 +1,5 @@
 const PORT = 3001;
-const DB_LINK = 'mongodb://127.0.0.1:27017/moviedb';
+const DB_LINK = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const TOKEN = 'mytokendonkey';
 module.exports = {
   PORT, DB_LINK, TOKEN,
