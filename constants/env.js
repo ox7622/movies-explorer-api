feb822f060b2cd7aadb57e3d93d946ad361534f1
Oxana Shovkoplyas <ekmati@yandex.ru>
2023-05-18 21:19:18 +0300

    final backend+ front v7

--- constants/env.js
+++ constants/env.js
@@ -1,7 +1,7 @@
 const PORT = 3001;
 const DB_LINK = 'mongodb://127.0.0.1:27017/bitfilmsdb';
 const TOKEN = 'mytokendonkey';
-const cookieDomain = 'api.mox.nomoredomains.work';
+const cookieDomain = '.mox.nomoredomains.work';
 
 // const cookieDomain = 'localhost:3001';
 
